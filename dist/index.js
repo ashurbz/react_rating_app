aa3771e33e7423d36bacb248aef166fb14e9eef5
Ashish <rbzashu1@gmail.com>
2024-05-12 12:33:17 +0530

    added readme and fix some bugs

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _Rating = _interopRequireDefault(require("./Rating"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var _default = exports.default = _Rating.default;
